d1767cce772eff4a642271696d53a3e4d34ada20
Miroslav <vasilevm4@gmail.com>
2022-04-05 22:11:38 +0300

    Updated to Net 0.6,and added administration dashboard,and appointment controller for administrators.

---
+++ OnlineCosmeticSalon.Web/Web/AspNetCoreTemplate.Web/Areas/Administration/Controllers/AppointmentsController.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCoreTemplate.Services.Data.Contracts;
+using AspNetCoreTemplate.Web.ViewModels.Appointments;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTemplate.Web.Areas.Administration.Controllers
+{
+    public class AppointmentsController : AdministrationController
+    {
+        private readonly IAppointmentsService appointmentsService;
+
+        public AppointmentsController(IAppointmentsService appointmentsService)
+        {
+            this.appointmentsService = appointmentsService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = new AppointmentsListViewModel
+            {
+                Appointments =
+                    await this.appointmentsService.GetAllAsync<AppointmentViewModel>(),
+            };
+            return this.View(viewModel);
+        }
+    }
+}
